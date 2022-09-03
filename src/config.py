692c11acef7ec3c9fb294ade7c8f650a6d74d9da
hnrtrng <thanhhai112004@gmail.com>
2022-09-03 22:36:38 +0700

    boss battle moi cho bai 11

--- src/config.py
+++ src/config.py
@@ -132,9 +132,9 @@ class ShadowBossConfig:
     ANIMATION_INTERVAL_MS: int = 200
     SPEED: int = 1
     DAMAGE: int = 1
-    INITIAL_HP: int = 100
+    INITIAL_HP: int = 150
 
-    ANGRY_INTERVAL_MS: int = 7000
+    ANGRY_INTERVAL_MS: int = 5000
     ANGRY_DURATION_MS: int = 2000
 
     HURT_DURATION_MS: int = 500
